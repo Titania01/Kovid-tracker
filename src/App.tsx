ec52a287da0045891bf8e8b3a83143de1bf87a81
Titania01 <adegbitebidmus@gmail.com>
2022-09-15 19:07:15 +0100

    added code base

--- src/App.tsx
+++ src/App.tsx
@@ -61,7 +61,10 @@ function App() {
               </div>
             </div>
             <div className="w-11/12 mx-auto bg-white mt-12 rounded-md">
-              <h2 className="text-black text-center p-4 font-bold text-2xl text-[#9972FF]">
+              <h2
+                className="text-center p-4 font-bold text-2xl 
+              text-[#9972FF]"
+              >
                 Confirmed cases across the 36 states and FCT
               </h2>
               <BarChart data={data} label={labels} />
